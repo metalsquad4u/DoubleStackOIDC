59d487b061be23ce0ace0232ded6e86b1a86fddb
Abdul <metalsquad4u@gmail.com>
2023-02-02 21:53:40 +0200

    Initial commit

---
+++ test/double_stack_oidc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DoubleStackOidc from '../lib/double_stack_oidc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/double_stack_oidc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DoubleStackOidc.DoubleStackOidcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
